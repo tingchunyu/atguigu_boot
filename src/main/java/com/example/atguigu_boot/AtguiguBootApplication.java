03cea7392eb8cf92f87e1807cbce0d5d2c5acab2
ljq <695398184@qq.com>
2022-08-06 13:46:20 +0800

    master commit2

---
+++ src/main/java/com/example/atguigu_boot/AtguiguBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.atguigu_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtguiguBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtguiguBootApplication.class, args);
+    }
+
+}
