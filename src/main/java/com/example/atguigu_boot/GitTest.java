6f190ba7344b1b46cc1db6e5b5f53a3f41239adf
ljq <695398184@qq.com>
2022-08-06 13:27:11 +0800

    second commit

--- src/main/java/com/example/atguigu_boot/GitTest.java
+++ src/main/java/com/example/atguigu_boot/GitTest.java
@@ -3,5 +3,7 @@ package com.example.atguigu_boot;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
